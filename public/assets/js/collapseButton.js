229eea6081997a0c53b8cc2a2e95dca07262bbe4
marcin <stanek.marcinp@gmail.com>
2018-06-06 15:53:17 +0200

    projects section finished

---
+++ public/assets/js/collapseButton.js
@@ -0,0 +1,9 @@
+$('#collapseButton').on('click', function(e) {
+  if($('#collapseButton').hasClass("expanded")){
+    $('#collapseButton').text('Rozwiń');
+    $('#collapseButton').removeClass("expanded");
+  } else {
+    $('#collapseButton').text('Zwiń');
+    $('#collapseButton').addClass("expanded");
+  }
+});
