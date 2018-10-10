6eff0d009308396e8c7aabbf8bc04713e7376bc3
Marcin Stanek <marcinstanek@MacBook-Pro-Marcin.local>
2018-10-10 10:43:41 +0200

    basic contact from added

--- public/_site/assets/js/collapseButton.js
+++ public/_site/assets/js/collapseButton.js
@@ -7,3 +7,14 @@ $('#collapseButton').on('click', function(e) {
     $('#collapseButton').addClass("expanded");
   }
 });
+
+
+$('#collapseButton').on('click', function(e) {
+  if($('#collapseButton').hasClass("expanded")){
+    $('#collapseButton').text('Rozwiń');
+    $('#collapseButton').removeClass("expanded");
+  } else {
+    $('#collapseButton').text('Zwiń');
+    $('#collapseButton').addClass("expanded");
+  }
+});
