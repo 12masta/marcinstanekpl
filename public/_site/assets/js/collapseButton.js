5c61646759f4c7676f8679df88c4f53d06f061cf
Marcin Stanek <marcinstanek@MacProMarcin.local>
2019-09-26 14:58:05 +0200

    .gitignore is now working

--- public/_site/assets/js/collapseButton.js
+++
@@ -1,9 +0,0 @@
-$('#collapseButton').on('click', function(e) {
-  if($('#collapseButton').hasClass("expanded")){
-    $('#collapseButton').text('Rozwiń');
-    $('#collapseButton').removeClass("expanded");
-  } else {
-    $('#collapseButton').text('Zwiń');
-    $('#collapseButton').addClass("expanded");
-  }
-});
